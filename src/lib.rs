347f65e2c39ad1c65a864744f74f808718a6dcde
maxleiko <max.tricoire@gmail.com>
2026-02-17 11:53:41 +0100

    update grammar

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use zed_extension_api::*;
 struct GreyCatExtension;
 
 impl GreyCatExtension {
-    fn lsp_bin_path(&self, worktree: &Worktree) -> String {
+    fn greycat_dir(worktree: &Worktree) -> PathBuf {
         let mut greycat_home = None;
         let mut home = None;
         for (name, value) in worktree.shell_env() {
@@ -19,16 +19,24 @@ impl GreyCatExtension {
                 _ => (),
             }
         }
-        let greycat_home = match (home, greycat_home) {
-            (Some(home), None) => PathBuf::from(home).join(".greycat"),
+        match (home, greycat_home) {
             (_, Some(greycat_home)) => PathBuf::from(greycat_home),
+            (Some(home), None) => PathBuf::from(home).join(".greycat"),
             (None, None) => PathBuf::from("/.greycat"),
-        };
-        greycat_home
-            .join("bin")
-            .join("greycat-lang")
-            .to_string_lossy()
-            .to_string()
+        }
+    }
+
+    fn lsp_bin_path(worktree: &Worktree) -> Result<String> {
+        let greycat_dir = Self::greycat_dir(worktree);
+        let lsp = greycat_dir.join("bin/greycat-lang");
+        if lsp.exists() {
+            return Ok(lsp.to_string_lossy().to_string());
+        }
+
+        Err(format!(
+            "unable to find bin/greycat-lang in {}",
+            greycat_dir.display()
+        ))
     }
 }
 
@@ -46,7 +54,7 @@ impl Extension for GreyCatExtension {
         worktree: &Worktree,
     ) -> Result<Command> {
         Ok(Command {
-            command: self.lsp_bin_path(worktree),
+            command: Self::lsp_bin_path(worktree)?,
             args: vec!["server".into(), "--stdio".into()],
             env: vec![],
         })
