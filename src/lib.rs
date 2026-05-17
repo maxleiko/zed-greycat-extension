19fc7d6bece610e9d2cd2e44430bdf05a686391c
maxleiko <max.tricoire@gmail.com>
2026-05-17 14:22:25 +0200

    v0.1.3: surfaced log level as user setting + bump grammar

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,37 @@
+use zed_extension_api::settings::LspSettings;
 use zed_extension_api::*;
 
 struct GreyCatExtension;
+
+const DEFAULT_LEVEL: &str = "info";
+
+fn resolve_level(settings: Option<&serde_json::Value>) -> &'static str {
+    let Some(value) = settings
+        .and_then(|v| v.get("level"))
+        .and_then(|v| v.as_str())
+    else {
+        return DEFAULT_LEVEL;
+    };
+    match value {
+        "off" => "off",
+        "info" => "info",
+        "debug" => "debug",
+        "trace" => "trace",
+        _ => DEFAULT_LEVEL,
+    }
+}
+
+fn build_rust_log(level: &str) -> String {
+    if level == "off" {
+        return "off".into();
+    }
+    [
+        format!("greycat_analyzer_server={level}"),
+        format!("greycat_analyzer_core={level}"),
+        format!("greycat_analyzer_analysis={level}"),
+    ]
+    .join(",")
+}
 
 impl Extension for GreyCatExtension {
     fn new() -> Self
@@ -12,16 +43,25 @@ impl Extension for GreyCatExtension {
 
     fn language_server_command(
         &mut self,
-        _language_server_id: &LanguageServerId,
+        language_server_id: &LanguageServerId,
         worktree: &Worktree,
     ) -> Result<Command> {
         // look for the new analyzer first
         worktree
             .which("greycat-analyzer")
-            .map(|command| Command {
-                command,
-                args: vec!["server".into()],
-                env: vec![],
+            .map(|command| {
+                let lsp_settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+                    .unwrap_or_default();
+                let level = resolve_level(lsp_settings.settings.as_ref());
+
+                Command {
+                    command,
+                    args: vec!["server".into()],
+                    env: vec![
+                        ("RUST_BACKTRACE".into(), "1".into()),
+                        ("RUST_LOG".into(), build_rust_log(level)),
+                    ],
+                }
             })
             // fallback to the old one if not found
             .or_else(|| {
@@ -32,6 +72,18 @@ impl Extension for GreyCatExtension {
                 })
             })
             .ok_or("unable to locate `greycat-analyzer` or `greycat-lang` in $PATH".into())
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<Option<serde_json::Value>> {
+        Ok(
+            LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+                .unwrap_or_default()
+                .initialization_options,
+        )
     }
 }
 
